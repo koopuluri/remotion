f9d46c5271fedd340629f645597edabb7c2973b1
Karthik Uppuluri <koopuluri@gmail.com>
2023-03-06 17:07:27 +0530

    reduce frame rate for specific animations

--- src/TermDefinition/Text/DancingWord.tsx
+++ src/TermDefinition/Text/DancingWord.tsx
@@ -2,11 +2,14 @@ import { Easing, interpolate, spring, useCurrentFrame, useVideoConfig } from "re
 import { random } from "remotion";
 
 const DancingCharacter = (props: {character: string, index: number}) => {
-  const frame = useCurrentFrame();
+
+  // Only animate every 10 frames (i.e. thrice per second)
+  let frame = Math.floor(useCurrentFrame() / 10) * 10;
+
   const {durationInFrames, fps} = useVideoConfig();
-  
+
   const skewAnimation = Math.cos(0.1 * frame)
-  let skew = interpolate(skewAnimation, [-1, 2], [-3, 3]);
+  let skew = interpolate(skewAnimation, [-1, 1], [-2, 2]);
 
   if (props.index % 2 === 0) {
     skew = -skew;
